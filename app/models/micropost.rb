f00c4ff7a27d90a7913b508a7f7c5c5a85177448
hide.naga <hh1120@hotmail.co.jp>
2020-05-25 10:45:38 +0000

    add favorites

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,9 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :liking_users, through: :reverses_of_favorite, source: :user
+
   validates :content, presence: true, length: { maximum: 255 }
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,32 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-
+  
+  has_many :favorites, dependent: :destroy
+  has_many :likes, through: :favorites, source: :micropost
+  
   has_many :microposts
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
 
+  def like(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unlike(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def liking?(micropost)
+    self.likes.include?(micropost)
+  end
+
+
+#follower, following
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
